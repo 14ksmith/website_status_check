28817bd6442aa96fe364db9256a16b7a69afd60a
14ksmith <ksmith2017@gmail.com>
2022-08-19 16:11:32 -0400

    Using perf_counter to see the time difference between the async and sync versions.

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from settings.open_settings import open_settings
-from url_request.url_request import request_url
-from requests.exceptions import HTTPError
+from url_request.url_request import get_websites_status
+import asyncio
+from time import perf_counter
 
 
-settings = open_settings()
-# Second return value from open_settings is the dict of websites. set urls to the values.
-urls = settings[1].values()
+if __name__ == "__main__":
 
-for url in urls:
-    print(url)
+    settings = open_settings()
+    # Second return value from open_settings is the dict of websites. set urls to the values.
+    urls = settings[1].values()
 
-    try:
-        request_url(url=url)
-        print("This website is working right now :)")
+    t1_start = perf_counter()
 
-    except HTTPError as e:
-        print(e)
-        print("This website is currently down.")
+    # ----------------------------- Async --------------------------------#
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_websites_status(urls=urls))
+    # --------------------------------------------------------------------#
+
+    # ----------------------------- Not Async ----------------------------#
+    # get_websites_status(urls=urls)
+    # --------------------------------------------------------------------#
+
+    t1_stop = perf_counter()
+
+    print(f"Elapsed time: {t1_stop - t1_start} seconds.")
--- url_request/url_request.py
+++ url_request/url_request.py
@@ -1,8 +1,67 @@
 import requests
+from requests.exceptions import HTTPError
+import asyncio
+from aiohttp import ClientSession, ClientResponseError
 
 
-def request_url(url):
+# -------------------------------- Async ------------------------------------------#
+
+
+async def request_url(session: ClientSession, url):
     """Send request for the given url. Raise the status of the request."""
 
-    response = requests.get(url=url)
+    response = await session.request("GET", url=url)
+
     response.raise_for_status()
+
+
+async def get_websites_status(urls):
+    """For each url provided, get the status of the request."""
+
+    # add a list of coroutines to complete
+    async with ClientSession() as session:
+
+        tasks = []
+
+        for url in urls:
+            print(url)
+
+            try:
+                tasks.append(request_url(session=session, url=url))
+                print("This website is working right now :)")
+
+            except ClientResponseError as e:
+                print(e)
+                print("This website is currently down.")
+
+        # execute all the tasks and wait for them to complete
+        asyncio.wait(tasks)
+
+
+# --------------------------------------------------------------------------------#
+
+
+# -------------------------- Not Async ------------------------------------------#
+
+# def request_url(url):
+#     """Send request for the given url. Raise the status of the request."""
+
+#     response = requests.get(url=url)
+
+#     response.raise_for_status()
+
+
+# def get_websites_status(urls):
+#     """For each url provided, get the status of the request."""
+
+#     for url in urls:
+#         print(url)
+
+#         try:
+#             request_url(url=url)
+#             print("This website is working right now :)")
+
+#         except HTTPError as e:
+#             print(e)
+#             print("This website is currently down.")
+# --------------------------------------------------------------------------------#
