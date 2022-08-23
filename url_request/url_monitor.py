6684f8e1d727cb1ca20322a3aefd09e02a4ea00e
14ksmith <ksmith2017@gmail.com>
2022-08-23 15:42:27 -0400

    Created new function separate from get_websites_status called request_all_urls, that will request the urls and return the output from each call as a list. This list can then be used to see which websites are down

--- url_request/url_monitor.py
+++ url_request/url_monitor.py
@@ -25,16 +25,15 @@ class ReadSettings:
 
 
 def send_website_down_email(websites_down, email_server):
-    """If there are websites down (in the websites_down list) send an email notification."""
+    """Send an email notification with websites down from websites_down list (ignore 'None')."""
 
-    if websites_down:
+    email_body = f"The following websites are currently down:\n\n"
 
-        email_body = f"The following websites are currently down:\n\n"
-
-        for website in websites_down:
+    for website in websites_down:
+        if website != "None":
             email_body += f"{website}\n"
 
-        send_email(email_body=email_body, server=email_server)
+    send_email(email_body=email_body, server=email_server)
 
 
 async def request_url(session: ClientSession, url):
@@ -44,19 +43,44 @@ async def request_url(session: ClientSession, url):
 
         response = await session.request("GET", url=url)
         response.raise_for_status()
-        print("This website is working right now :)")
+        print(f"{url} is working right now :)")
 
     except ClientResponseError as error:
-        # print(error)
-        if error.status == 403:
-            # websites_down.append(url)
-            print("This website is forbidden.")
+
+        if error.status == 301:
+            print(url, "Permanent Redirect", error.status)
+
+        elif error.status == 302:
+            print(url, "Temporary Redirect", error.status)
+
+        elif error.status == 403:
+            print(url, "Website Forbidden", error.status)
+            return url
 
         elif error.status == 404:
-            pass
+            print(url, "Website Not Found", error.status)
+
+        elif error.status == 410:
+            print(url, "Website Gone", error.status)
+
+        elif error.status == 500:
+            print(url, "Internal Server Error, Website Down", error.status)
+            return url
+
+        elif error.status == 503:
+            print(url, "Service Unavailable, Website Down", error.status)
+            return url
 
         else:
             print(url, "unhandled error code", error.status)
+
+
+async def request_all_urls(session: ClientSession, urls):
+    """Request all urls in settings and return websites that are down."""
+    output = await asyncio.gather(
+        *[request_url(session=session, url=url) for url in urls]
+    )
+    return output
 
 
 async def get_websites_status():
@@ -69,17 +93,11 @@ async def get_websites_status():
         # add a list of coroutines to complete
         async with ClientSession() as session:
 
-            websites_down = []
+            websites_down = await request_all_urls(
+                session=session, urls=settings.websites_to_check
+            )
 
-            tasks = []
-
-            for url in settings.websites_to_check:
-                print(url)
-                tasks.append(request_url(session=session, url=url))
-
-            # execute all the tasks and wait for them to complete
-            await asyncio.wait(tasks)
-
+        print(websites_down)
         send_website_down_email(
             websites_down=websites_down, email_server=settings.email_server
         )
